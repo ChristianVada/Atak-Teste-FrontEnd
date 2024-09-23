8f6171b09fe52bd6b3f3a014de27ff016a6d9a6e
Christian Vada <christianryuji@hotmail.com>
2024-09-23 02:35:53 -0300

    novos dados para cadastrar

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,35 +1,62 @@
 import { createContext, useState } from "react";
 import apiUser from "../services/api";
 import { useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [token, setToken] = useState("");
   const navigate = useNavigate();
+  const toast = useToast();
+
+  const [token, setToken] = useState("");
 
   const fazerLogin = async (dataLogin) => {
     try {
       const response = await apiUser.post("/api/autenticacao/login", dataLogin);
       setToken(response.data.token);
       navigate("/encaminharEmail");
+      toast({
+        title: "Usuário conectado",
+        position: "top-right",
+        isClosable: true,
+        status: "success",
+        duration: 3000,
+      });
     } catch (error) {
+      toast({
+        title: "Email e/ou senha estão incorretos",
+        position: "top-right",
+        isClosable: true,
+        status: "error",
+        duration: 3000,
+      });
       console.warn(error);
     }
   };
 
-  const EnviarEmailComExcel = async (quantidade) => {
+  const EnviarEmailComExcel = async (dataEmail) => {
     try {
-      await apiUser.post(
-        `/api/excel/enviar-email?quantidade=${quantidade}`,
-        null,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      await apiUser.post(`/api/excel/enviar-email`, dataEmail, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      toast({
+        title: "Email enviado com sucesso",
+        position: "top-right",
+        isClosable: true,
+        status: "success",
+        duration: 3000,
+      });
     } catch (error) {
+      toast({
+        title: "Ops algo deu errado, tente logar novamente",
+        position: "top-right",
+        isClosable: true,
+        status: "error",
+        duration: 3000,
+      });
       console.warn(error);
     }
   };
@@ -38,7 +65,21 @@ const UserProvider = ({ children }) => {
     try {
       await apiUser.post(`/api/autenticacao/registro`, dataRegister);
       navigate("/login");
+      toast({
+        title: "Cadastrado com sucesso",
+        position: "top-right",
+        isClosable: true,
+        status: "success",
+        duration: 3000,
+      });
     } catch (error) {
+      toast({
+        title: "Email já cadastrado",
+        position: "top-right",
+        isClosable: true,
+        status: "error",
+        duration: 3000,
+      });
       console.warn(error);
     }
   };
--- src/pages/EncaminharEmail/index.jsx
+++ src/pages/EncaminharEmail/index.jsx
@@ -1,41 +1,92 @@
 import {
   Box,
-  Button,
   FormControl,
   FormLabel,
   Input,
-  Text,
   Textarea,
+  Stack,
+  Button,
 } from "@chakra-ui/react";
 import { useContext, useState } from "react";
 import { UserContext } from "../../context/userContext";
 
-function EncaminharEmail() {
+const EncaminharEmail = () => {
   const { EnviarEmailComExcel } = useContext(UserContext);
-  const [inputValue, setInputValue] = useState("");
 
-  const HandleSubmit = () => {
-    EnviarEmailComExcel(inputValue);
-  };
+  const [quantidade, setQuantidade] = useState("");
+  const [destinatario, setDestinatario] = useState("sergio.junior@atak.com.br");
+  const [assunto, setAssunto] = useState("Atak Teste - Dados Gerados");
+  const [corpoEmail, setCorpoEmail] = useState(
+    "Este é um projeto de geração de dados fictícios. Confira o código no repositório: <https://github.com/ChristianVada/Atak-Teste-BackEnd>, <https://github.com/ChristianVada/Atak-Teste-FrontEnd>"
+  );
 
-  const HandleChange = (evento) => {
-    setInputValue(evento.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const dataEmail = {
+      Quantidade: quantidade,
+      Destinatario: destinatario,
+      Assunto: assunto,
+      CorpoEmail: corpoEmail,
+    };
+    EnviarEmailComExcel(dataEmail);
   };
 
   return (
-    <Box maxW="md" mx="auto" mt={10} p={5} borderWidth={1} borderRadius="lg">
-      <Text mb={4}>Os dados gerados serão encaminhados para o email xxx.</Text>
-      <FormLabel>Quantidade de estar entre 10 e 1000</FormLabel>
-      <Input
-        value={inputValue}
-        placeholder="Defina a quantidade de cliente fictícios a ser gerado"
-        onChange={HandleChange}
-      ></Input>
-      <Button colorScheme="blue" mt={4} onClick={HandleSubmit}>
-        Confirmar
-      </Button>
+    <Box maxW="sm" mx="auto" mt={10} p={5} borderWidth={1} borderRadius="lg">
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={4}>
+          <FormControl id="quantidade">
+            <FormLabel>Quantidade de clientes fictícios a ser gerado</FormLabel>
+            <Input
+              type="number"
+              value={quantidade}
+              onChange={(e) => setQuantidade(e.target.value)}
+              required
+              min={10}
+              max={1000}
+              placeholder="Digite a quantidade"
+            />
+          </FormControl>
+
+          <FormControl id="destinatario">
+            <FormLabel>Destinatário</FormLabel>
+            <Input
+              type="email"
+              value={destinatario}
+              onChange={(e) => setDestinatario(e.target.value)}
+              required
+              placeholder="Digite o email do destinatário"
+            />
+          </FormControl>
+
+          <FormControl id="assunto">
+            <FormLabel>Assunto</FormLabel>
+            <Input
+              type="text"
+              value={assunto}
+              onChange={(e) => setAssunto(e.target.value)}
+              required
+              placeholder="Digite o assunto"
+            />
+          </FormControl>
+
+          <FormControl id="corpoEmail">
+            <FormLabel>Corpo do Email</FormLabel>
+            <Textarea
+              value={corpoEmail}
+              onChange={(e) => setCorpoEmail(e.target.value)}
+              required
+              placeholder="Digite o corpo do email"
+            />
+          </FormControl>
+
+          <Button type="submit" colorScheme="teal" size="lg" fontSize="md">
+            Enviar Email
+          </Button>
+        </Stack>
+      </form>
     </Box>
   );
-}
+};
 
 export default EncaminharEmail;
